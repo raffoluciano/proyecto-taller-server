2ee0db359e1b835627c338b35fd71ed208f79bfd
raffoluciano <42227702+raffoluciano@users.noreply.github.com>
2022-05-18 17:49:06 -0300

    .

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     db: {
         host:'localhost',
         user: 'postgres',
-        password: 'dpb2lsda',
+        password: '',
         database: 'TP_Final_Taller_2.5',
         port:'5432'
     }
