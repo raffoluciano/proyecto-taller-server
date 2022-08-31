2be0e1d87df68ff11bb595f024044e8c5393715a
raffoluciano <42227702+raffoluciano@users.noreply.github.com>
2022-08-31 20:25:59 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 
 const users = require('./routes/user');
 const Destinations = require('./routes/destiny');
