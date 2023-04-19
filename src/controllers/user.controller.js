d37e7ab557a16131a01e1773ff0582e3a932d5c0
fercafferata <fercafferata@outlook.com>
2023-04-18 21:28:00 -0300

    Login

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const {Pool} =require('pg');
 const config = require('../../config');
-
+//const cookieParser = require('cookie-parser');
+//const bcrypt = require('bcrypt');
 const pool = new Pool(config.db);
 
 const getUsers = async (req, res)=> {
@@ -47,11 +48,42 @@ const updateUser =async (req,res) =>{
     res.json(`User ${dni} updated successfully`);
 };
 
+const loginUser = async(req, res) =>{
+
+// Ruta para el inicio de sesión
+  try {
+    // Obtén los datos del formulario de inicio de sesión del cuerpo de la solicitud
+    const { email, password } = req.body;
+
+    // Verifica en la base de datos si el usuario existe y tiene una contraseña válida
+    const query = `SELECT * FROM usuario WHERE email = $1 AND contrasena = $2`;
+    const values = [email, password];
+    const result = await pool.query(query, values);
+
+    // Si el usuario existe y la contraseña es válida
+    if (result.rows.length === 1) {
+      const { dni, nombre, rol } = result.rows[0];
+      // Envía la respuesta con los datos del usuario y el rol
+      res.status(200).json({ dni, nombre, rol });
+
+    } else {
+      // Si el usuario no existe o la contraseña es incorrecta
+      // Envía una respuesta con un mensaje de error
+      res.status(401).json({ mensaje: 'Credenciales inválidas' });
+    }
+  } catch (error) {
+    // Si ocurre un error en la consulta a la base de datos
+    // Envía una respuesta con un mensaje de error
+    console.log(error);
+    res.status(500).json({ mensaje: 'Error en el servidor' });
+  }
+}
+
     module.exports = {
         getUsers,
         getUsersById,
         createUser,
         deleteUser,
-        updateUser
-        
-    }
+        updateUser,
+        loginUser
+      }
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,12 +1,15 @@
 const {Router} = require('express');
 const router = Router();
+const bodyParser = require('body-parser');
 
-const {getUsers,createUser,getUsersById,deleteUser,updateUser}=require('../controllers/user.controller')
+const {getUsers,createUser,getUsersById,deleteUser,updateUser,loginUser}=require('../controllers/user.controller')
 
+router.use(bodyParser.json());
 //user routes
 router.get('/user', getUsers);
 router.get('/:id', getUsersById);
 router.post('/user',createUser);
+router.post('/login', loginUser);
 router.delete('/user/:id', deleteUser);
 router.put('/user', updateUser);
 
